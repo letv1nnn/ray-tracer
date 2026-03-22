fc1eb8f099cedc20f884cb2e63e6d9c90621f058
letv1nnn <letv1n2007@icloud.com>
2026-03-22 00:36:03 +0000

    added a sphere

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use ray_tracer::{
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // image
-    const ASPECT_RATIO: f64 = 16. / 9.;
+    const ASPECT_RATIO: f64 = 16.0 / 9.0;
     let image_width = 400;
 
     // calculate the image height
@@ -36,7 +36,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // calculate the location of the upper left pixel.
     let viewport_upper_left =
         camera_center - Vec3::new(0., 0., focal_length) - viewport_u / 2. - viewport_v / 2.;
-    let pixel100_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+    let pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
     write!(
         std::io::stdout(),  // P3 means colors are in ASCII
@@ -48,7 +48,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     for i in 0..image_height {
         for j in 0..image_width {
             let pixel_center =
-                pixel100_loc + (pixel_delta_u * i as f64) + (pixel_delta_v * j as f64);
+                pixel00_loc + (pixel_delta_u * i as f64) + (pixel_delta_v * j as f64);
             let ray_direction = pixel_center - camera_center;
             let ray = Ray::new(camera_center, ray_direction);
 
@@ -62,7 +62,19 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
+fn hit_sphere(center: Vec3, radius: f64, ray: &Ray) -> bool {
+    let oc = center - ray.origin();
+    let a = ray.direction().dot(&ray.direction());
+    let b = -2.0 * ray.direction().dot(&oc);
+    let c = oc.dot(&oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+    discriminant >= 0.0
+}
+
 fn ray_color(ray: &Ray) -> ColorRGB {
+    if hit_sphere(Vec3::from([0., 0., -1.]), 0.5, &ray) {
+        return ColorRGB::new(1., 0., 0.);
+    }
     let unit_direction = ray.direction().try_unit().unwrap_or(Vec3::ZERO);
     let a = 0.5 * (unit_direction.y() + 1.);
     (1.0 - a) * ColorRGB::ONE + a * ColorRGB::new(0.5, 0.7, 1.)
